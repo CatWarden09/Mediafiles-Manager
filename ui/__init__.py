a0249eb61dffb0bc706caa3cd7dd74705168600a
CatWarden09 <CatWarden09@proton.me>
2026-01-28 21:28:13 +0400

    Refactor project structure:
    Moved the MainWindow and ThumbCreationThread classes to the ui folder. Moved the debug constant to config.py.

--- ui/__init__.py
+++ ui/__init__.py
@@ -5,3 +5,4 @@ from .error_window import ErrorWindow
 from .tags_setting_windows import TagsSettingsWindow, ItemTagsSettingsWindow
 from .tags_list import TagsList
 from .preview_window import PreviewWindow
+from .main_window import MainWindow
